a8a8b87bd4147eb86b937c42b278448a53450acc
Akash Rathod <akash.rathod162844@gmail.com>
2026-02-14 18:29:25 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,29 @@
+first_name = "Akash"
+last_name = "Rathod"
+full_name = first_name + " " + last_name
+print(full_name)
+
+# repetition
+message = "This is a Warning! "
+print(message*10)
+
+print(message.upper())
+print(message.lower())
+print(message.strip())
+print(message.replace("Warning", "Error"))
+
+name = '''Akash Rathod said "hello"
+        sanju said "hi"
+        '''
+print(len(message)) # length
+
+name = "Akash Rathod"
+print(name[4]) # index = position - 1
+print(name[2:6])
+print(name[2:])
+print(name[:4])
+print(name[-2])
+print(name[::3])
+
+s = "Akash Rathod\tis a good boy"
+print(s)
