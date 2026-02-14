ae9cd3b513823d6316b075e15d643e31cf3b0e21
Akash Rathod <akash.rathod162844@gmail.com>
2026-02-14 18:36:07 +0530

    Update exercise1.py

---
+++ 4.py
@@ -0,0 +1,13 @@
+# name = "Tony Stark"
+# age = 51
+# thinking_level = "Genius"
+# print(name,age,thinking_level)
+
+# print("name : Tony stark ,age:51 years old he is genius")
+
+First_name = "Tony"
+Last_name = "Stark"
+age = 51
+is_genius = True
+
+print(First_name+" " +Last_name,age,is_genius)
