f06e4d1c4a8bef3bb221ba8f6cd970af85f37699
Akash Rathod <akash.rathod162844@gmail.com>
2026-02-15 20:21:13 +0530

    Create swaping_two_variable.py

---
+++ swaping_two_variable.py
@@ -0,0 +1,31 @@
+# swap two variable useing traditional method using temp
+a = 5
+b = 6
+temp = a
+a = b
+b = temp
+print(f"a = {a}, b = {b}")
+
+# swaping while taking input from user
+a = int(input("Enter a number a:"))
+b = int(input("Enter a number b:"))
+temp = a
+a = b
+b = temp
+print(f"a = {a}, b = {b}")
+
+# swapping two variables without using temp
+a = int(input("Eneter a number a: "))
+b = int(input("Eneter a number b: "))
+print(a,b)
+a,b = b,a
+print(f"after swapping a: {a}, after swapping b: {b}")
+
+# swaping two numbers without using temp
+# a = int(input("Enter a number a:"))
+# b = int(input("Enter a number b:"))
+# a = abs(a - b)
+# b = abs(a + b)
+# a = abs(a - b)
+# print(f"a = {a}, b = {b}")
+i'm still working on this code later i will fix it
