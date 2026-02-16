a8c80fad02af299ffc4c17928ede1ed94118c650
Akash Rathod <akash.rathod162844@gmail.com>
2026-02-16 20:42:07 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,15 @@
+# to find largest of two numbers
+ a,b = 3,4
+if a>b:
+  print("a is greater")
+else:
+  print("b is greater")
+
+# to find largest of three numbers
+a,b,c = 3,4,5
+if a>b and a>c:
+  print("a is greater")
+elif b>c:
+  print("b is greater")
+else:
+  print("c is greater")
