421288da4296e8fb79ec118f33b19a3c361458b6
Akash Rathod <akash.rathod162844@gmail.com>
2026-02-16 20:24:18 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,8 @@
+# for loop
+lis = [1,2,3,4,5,6]
+for i in lis:
+  print(lis)  # it returns the lis like 1 in single line next 2 in single line
+
+a = input(int("enter a number "))
+lis.append(a)
+print(lis) # it adds lis in the value entered by user
