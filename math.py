5cbe7299417ebf09e6e1ade80c6eab827ebde957
Akash Rathod <akash.rathod162844@gmail.com>
2026-02-19 22:34:59 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+import math
+print(math.pi) # it gives the pi value
+
+print(math.e)# it gives the exponantion constant
+
+# ----->>> SQRT FUNTCION
+a = 9
+result = math.sqrt(a)
+print(result)
+print(math.sqrt(9))
+#--->CEIL FUNCTION ROUND UP THE VALUE 
+a = 8.1
+print(math.ceil(a))
+#--->floor function round down the values
+a = 8.9
+print(math.floor(a))
