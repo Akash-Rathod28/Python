53c27bcfca43c74f1ab7526f0216de885111a1f2
Akash Rathod <akash.rathod162844@gmail.com>
2026-02-21 20:03:59 +0530

    Create oops_circle_AREA_PERIMETER.py

---
+++ oops_circle_AREA_PERIMETER.py
@@ -0,0 +1,11 @@
+import math
+class Circle:
+    def __init__(self,radius):
+        self.radius = radius
+        self.a = (math.pi * math.pow(self.radius,2))
+    def __init__(self,perimeter):
+        self.perimeter = perimeter
+        self.b = 2 * math.pi * c1(self.radius)
+c1 = Circle(8)
+print(c1.a)
+print(c1.b)
