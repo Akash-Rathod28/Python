997acd7b5b02b2764bc3651a1f3e2827f2e416f9
Akash Rathod <akash.rathod162844@gmail.com>
2026-03-19 21:32:12 +0530

    Create About Strings.py

---
+++ About Strings.py
@@ -0,0 +1,69 @@
+a = "Udemy Physics Walaha" # Storing a value in the variable name called a
+
+print(a.upper()) # printing the value all the letters should be in capital if it is lower converted all char to upper
+
+print("---------------")
+
+print(a.lower()) # converting upper ---> lower
+
+print("---------------")
+
+print(a.capitalize()) # converting first letter to capital
+
+print("---------------")
+
+print(a.swapcase()) # converting lower --> upper upper ---> lower
+
+print("---------------")
+
+print(a.title()) # converting each word starting letter capital rest small
+print("---------------")
+
+s = "akash"
+
+print(s.isalpha()) # prints True if the string is only alphabets
+print("---------------")
+s = "893674" 
+print(s.isdigit()) # prints True if the string as numbers
+
+print("---------------")
+s = "Akash893"
+print(s.isalnum()) # prints the string contain both alpha and numeric
+
+print("---------------")
+s = "_akash"
+
+print(s.isidentifier()) # checks the entered string is valid indentifier(variable name) or not
+
+print("---------------")
+
+s = "  "
+
+print(s.isspace()) # checks the string contain only spaces
+
+print("---------------")
+s = "Akash Rathod"
+
+print(s[::-1]) # it reverse the string
+
+print("---------------")
+
+print(s[1:5]) # it prints the substring of string from 0 to 5-1
+
+print("---------------")
+
+print(s[0]) # it prints the first index of the string
+
+print("---------------")
+
+print(s[-1]) # it prints the last index of the string 
+
+print("---------------")
+s = "Akash Rathod"
+print(s.startswith("A"))
+
+print("---------------")
+
+
+
+
